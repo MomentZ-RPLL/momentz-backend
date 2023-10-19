952992ff9f977bf4cc6a2c2f3da7f54b432445d6
quitsv <axellsilvano29@gmail.com>
2023-10-19 21:31:50 +0700

    upload profile picture function for register

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,20 +1,18 @@
-require('dotenv').config();
-const secretKey = process.env.JWT_SECRET_KEY;
+require('dotenv').config()
+const secretKey = process.env.JWT_SECRET_KEY
 
 const UserModels = require('../models/users')
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken')
 
 function generateToken(user) {
-    const payload = { username: user.username };
-    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
-    return token;
+    const payload = { username: user.username }
+    const token = jwt.sign(payload, secretKey, { expiresIn: '1h' })
+    return token
 }
-
 
 exports.registerUser = async (req, res) => {
     try {
-        const data = req.body
-        const [result] = await UserModels.registerUser(data)
+        const [result] = await UserModels.registerUser(req)
 
         res.status(200).json({
             status: '200',
@@ -29,19 +27,19 @@ exports.registerUser = async (req, res) => {
 }
 
 exports.loginUser = async (req, res) => {
-        const data = req.body;
-        const user = await UserModels.loginUser(data);
-        if (user) {
-            const token = generateToken(user);
-            res.status(200).json({
-                status: '200',
-                message: 'Login user success',
-                data: {
-                    token: token,
-                    user: user
-                }
-            })
-        } else {
-            res.status(401).json({ message: 'Invalid username or password' });
-        }
+    const data = req.body
+    const user = await UserModels.loginUser(data)
+    if (user) {
+        const token = generateToken(user)
+        res.status(200).json({
+            status: '200',
+            message: 'Login user success',
+            data: {
+                token: token,
+                user: user
+            }
+        })
+    } else {
+        res.status(401).json({ message: 'Invalid username or password' })
+    }
 }
