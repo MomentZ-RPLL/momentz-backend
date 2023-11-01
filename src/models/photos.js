45273d6e4aaae567023c9b6e16e709642f12d7d3
quitsv <axellsilvano29@gmail.com>
2023-11-02 00:47:52 +0700

    minor fix

--- src/models/photos.js
+++ src/models/photos.js
@@ -2,25 +2,19 @@ const dbPool = require('../config/database')
 const ErrorResponse = require('../utils/errorResponse')
 
 exports.postMedia = async (data) => {
-  try {
-
     if (data.file === undefined) {
-      throw new ErrorResponse(400, 'file is required')
+        throw new ErrorResponse(400, 'file is required')
     }
     if (data.body.caption === undefined) {
-      data.body.caption = ""
+        data.body.caption = ""
     }
 
     const query = `INSERT INTO posts SET ?`
     const value = {
-      id_user: data.user.id_user,
-      caption: data.body.caption,
-      post_media: data.file.filename,
-      created_at: data.body.created_at
+        id_user: data.user.id_user,
+        caption: data.body.caption,
+        post_media: data.file.filename,
+        created_at: data.body.created_at
     }
-
     return await dbPool.query(query, value)
-  } catch (error) {
-    throw new ErrorResponse(500, error.message)
-  }
 }
