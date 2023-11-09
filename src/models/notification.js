4126fa37fffa88cfea205b48aa11b8008ab0fcbb
ShanChrist <shandychristian70@gmail.com>
2023-11-09 13:15:35 +0700

    bug fix

--- src/models/notification.js
+++ src/models/notification.js
@@ -17,7 +17,7 @@ exports.getNotifLikes = async (id_post) => {
 
 exports.getNotifFollow = async (id_user) => {
     const query = 'SELECT count(is_notified) AS LikesNotification FROM post_likes WHERE id_post= ? AND is_notified= ?'
-    const data = await dbPool.query(query, [id_post, 1])
+    const data = await dbPool.query(query, [id_user, 1])
 
     return data
 }
